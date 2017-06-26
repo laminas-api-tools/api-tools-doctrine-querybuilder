49665dd8607a8d31a892aaf5dc3464c8ac2a6c8f
Tom H Anderson <tom.h.anderson@notevault.com>
2017-06-26 11:33:10 -0600

    Set Zend (c) to 2017

--- src/Filter/ORM/LeftJoin.php
+++ src/Filter/ORM/LeftJoin.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @license   http://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
- * @copyright Copyright (c) 2014 Zend Technologies USA Inc. (http://www.zend.com)
+ * @copyright Copyright (c) 2017 Zend Technologies USA Inc. (http://www.zend.com)
  */
 
 namespace ZF\Doctrine\QueryBuilder\Filter\ORM;
