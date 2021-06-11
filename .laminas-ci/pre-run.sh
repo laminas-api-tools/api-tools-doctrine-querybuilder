09dcf38c59758e66f5d3513ac41f0e913772b4a8
Matthew Weier O'Phinney <matthew@weierophinney.net>
2021-06-11 11:21:54 -0500

    qa: ignore platform reqs when installing mongo client library under PHP 8
    Signed-off-by: Matthew Weier O'Phinney <matthew@weierophinney.net>

--- .laminas-ci/pre-run.sh
+++ .laminas-ci/pre-run.sh
@@ -2,4 +2,12 @@
 
 set -e
 
-composer require --dev --no-interaction --prefer-dist alcaeus/mongo-php-adapter
+JOB=$3
+PHP_VERSION=$(echo "${JOB}" | jq -r '.php')
+
+if [[ "$PHP_VERSION" =~ 8. ]];then
+    composer require --ignore-platform-req=php --dev --no-interaction --prefer-dist alcaeus/mongo-php-adapter
+else
+    composer require --dev --no-interaction --prefer-dist alcaeus/mongo-php-adapter
+fi
+
